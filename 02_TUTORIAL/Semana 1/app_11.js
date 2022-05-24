6aa6082c51b27de5e2f88cc4ea5f0ae19cf98f1f
brunomleao <bruno.leao@sou.inteli.edu.br>
2022-05-24 10:10:17 -0300

    atualizando

---
+++ 02_TUTORIAL/Semana 1/app_11.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const hostname = "127.0.0.1";
+const port = 3011;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end(
+    '<!DOCTYPE html>\
+    <html lang="en">\
+    <head> \
+      <meta charset="utf-8"> \
+      <meta name="viewport" content="width=device-width, initial-scale=1"> \
+      <title>Etapa 0 - INSTALACAO</title> \
+    </head> \
+    <body>\
+      <div id="main"> \
+            <h1> Etapa 1 - INSTALACAO - Servidor Node.js </h1> \
+            <H2> Meu servidor NODE.js funciona!</H2> </div> \
+    </body> \
+    </html>'
+  );
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
